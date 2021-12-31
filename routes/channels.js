ed1ea33718cd01b99cd6d5d93337208ac5954013
carabila <andrea.carabillo@gmail.com>
2021-12-31 19:50:44 +0100

    code cleanup

--- routes/channels.js
+++ routes/channels.js
@@ -2,11 +2,9 @@ const express = require('express');
 var router = express.Router();
 const dreambox = require('../dreambox-ctrl');
 const config = require('../config');
-const ffmpegUtils = require('../ffmpeg-utils.js')
 
 router.get('/', function(req, res, next){
     let response = res;
-    ffmpegUtils.ffmpegCleanup();
 
     dreambox.init(config.host).then((ret) => {
         dreambox.getBouquets().then((ret) => {
